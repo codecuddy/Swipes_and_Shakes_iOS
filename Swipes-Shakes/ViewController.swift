63add1d6a241f90c3e4c88566c578748e965b48a
Charlie <charliecuddy4@gmail.com>
2018-01-11 13:22:25 -0600

    shake function added

--- Swipes-Shakes/ViewController.swift
+++ Swipes-Shakes/ViewController.swift
@@ -15,6 +15,16 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view, typically from a nib.
     }
 
+    override func motionEnded(_ motion: UIEventSubtype, with event: UIEvent?) {
+        
+        if event?.subtype == UIEventSubtype.motionShake {
+            
+            print("Device was shaken!")
+            
+        }
+        
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
