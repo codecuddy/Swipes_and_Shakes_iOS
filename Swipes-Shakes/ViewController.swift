1523a17ea259935f4bda1f9c0bce33ce6ecdd55a
Charlie <charliecuddy4@gmail.com>
2018-01-11 13:29:58 -0600

    swipe left and right functions added

--- Swipes-Shakes/ViewController.swift
+++ Swipes-Shakes/ViewController.swift
@@ -13,6 +13,17 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        let swipeRight = UISwipeGestureRecognizer(target: self, action: #selector(ViewController.swiped(gesture:)))
+        
+        swipeRight.direction = UISwipeGestureRecognizerDirection.right
+        self.view.addGestureRecognizer(swipeRight)
+        
+        let swipeLeft = UISwipeGestureRecognizer(target: self, action: #selector(ViewController.swiped(gesture:)))
+        
+        swipeLeft.direction = UISwipeGestureRecognizerDirection.left
+        self.view.addGestureRecognizer(swipeLeft)
+        
     }
 
     override func motionEnded(_ motion: UIEventSubtype, with event: UIEvent?) {
@@ -20,6 +31,27 @@ class ViewController: UIViewController {
         if event?.subtype == UIEventSubtype.motionShake {
             
             print("Device was shaken!")
+            
+        }
+        
+    }
+    
+    @objc func swiped(gesture: UIGestureRecognizer) {
+        
+        if let swipeGesture = gesture as? UISwipeGestureRecognizer {
+            
+            switch swipeGesture.direction {
+                
+            case UISwipeGestureRecognizerDirection.right:
+                print("User Swiped Right")
+                
+            case UISwipeGestureRecognizerDirection.left:
+                print("User Swiped Left")
+                
+            default:
+                break
+                
+            }
             
         }
         
